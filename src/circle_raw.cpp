2e358a2bb2bdb144670816b18f7a8a1ccfc18d04
alanypf <alanypf@gmail.com>
2022-04-10 08:32:18 +0800

    setpoint_raw

---
+++ src/circle_raw.cpp
@@ -0,0 +1,126 @@
+//
+// Created by ypf on 2021/12/7.
+//
+
+// offboard example connected to Airsim
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/PositionTarget.h>
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<mavros_msgs::PositionTarget>
+            ("mavros/setpoint_raw/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(50.0);
+
+    //
+    double circle_rate = 40;
+    double t_step = 1.0/ circle_rate;
+
+    // parameters for circle
+    double wn = 0.8;
+    double r = 2;
+    double counter = 0;
+
+    // circle center
+    float xc0 = 0;
+    float yc0 = 0;
+    float zc0 = 2;
+
+    // wait for FCU connection
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+//    geometry_msgs::PoseStamped pose;
+//    pose.pose.position.x = xc0;
+//    pose.pose.position.y = yc0 -r;
+//    pose.pose.position.z = zc0;
+
+    mavros_msgs::PositionTarget pos_target_raw;
+    pos_target_raw.coordinate_frame = 1;
+    pos_target_raw.type_mask = 0b100111111000;
+    pos_target_raw.position.x = xc0;
+    pos_target_raw.position.y = yc0 - r;
+    pos_target_raw.position.z = zc0;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pos_target_raw);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+                ROS_INFO("!");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+//        geometry_msgs::PoseStamped circle_pose;
+//
+//        circle_pose.pose.position.x = xc0 - r*sin(wn*counter*t_step);
+//        circle_pose.pose.position.y = yc0 - r*cos(wn*counter*t_step);
+//        circle_pose.pose.position.z = zc0;
+
+        mavros_msgs::PositionTarget  circle_pose_raw;
+        circle_pose_raw.coordinate_frame = 1;
+        circle_pose_raw.type_mask = 0b100111111000;
+        circle_pose_raw.position.x = xc0 - r*sin(wn*counter*t_step);
+        circle_pose_raw.position.y = yc0 - r*cos(wn*counter*t_step);
+        circle_pose_raw.position.z = zc0;
+
+        local_pos_pub.publish(circle_pose_raw);
+
+        ros::spinOnce();
+        counter++;
+        rate.sleep();
+    }
+
+    return 0;
+}
+
+
